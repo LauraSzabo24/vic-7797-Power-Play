8a6926a5fa86abd80b711d270478bf9180e3b93e
samftc <chenxiang.xu56@k12.leanderisd.org>
2023-01-15 20:55:31 -0600

    Blueberry Pancakes

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/SamAutoMM.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/SamAutoMM.java
@@ -11,33 +11,44 @@ public class SamAutoMM {
         Pose2d startPose = new Pose2d(-35, -60, Math.toRadians(90));
         Pose2d farmingPose = new Pose2d(-30.4,-6,Math.toRadians(45));
         Pose2d approachPose = new Pose2d(-35.4,-11.3,Math.toRadians(45));
-        Pose2d stackPose = new Pose2d(60, -12, Math.toRadians(0));
+
+        Pose2d stackPose = new Pose2d(-60, -12, Math.toRadians(180));
 
         //-60.8,-35.6
-        Pose2d middlePark = new Pose2d(-35.8,-12,Math.toRadians(0));
-        Pose2d leftPark =  new Pose2d(-57.8,-12,Math.toRadians(0));
-        Pose2d rightPark =  new Pose2d(-10.8,-12,Math.toRadians(270));
+        Pose2d middlePark = new Pose2d(-35.8,-12,Math.toRadians(90));
+        Pose2d leftPark =  new Pose2d(-57.8,-12,Math.toRadians(90));
+        Pose2d rightPark =  new Pose2d(-10.8,-12,Math.toRadians(90));
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(63, 50, Math.toRadians(70), Math.toRadians(70), 13)
+                .setConstraints(63, 50, Math.toRadians(100), Math.toRadians(100), 13)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(startPose)
-                                .lineToLinearHeading(approachPose)
+                                .lineToLinearHeading(new Pose2d(-35.4,-11.3,Math.toRadians(90)))
+                                .turn(Math.toRadians(-45))
                                 .lineToLinearHeading(farmingPose)
                                 .waitSeconds(0.5)
                                 .lineToLinearHeading(approachPose)
-                                .lineToSplineHeading(stackPose)
-                                .waitSeconds(1)
-                                .lineToSplineHeading(approachPose)
+                                .turn(Math.toRadians(135))
+                                .lineToLinearHeading(stackPose)
+                                .waitSeconds(0.8)
+                                .lineToLinearHeading(new Pose2d(-35.4,-11.3,Math.toRadians(180)))
+                                .turn(Math.toRadians(-135))
                                 .lineToLinearHeading(farmingPose)
                                 .waitSeconds(0.5)
                                 .lineToLinearHeading(approachPose)
-                                .lineToSplineHeading(stackPose)
-                                .waitSeconds(1)
-                                .lineToSplineHeading(approachPose)
+                                .turn(Math.toRadians(135))
+                                .lineToLinearHeading(stackPose)
+                                .waitSeconds(0.8)
+                                .lineToLinearHeading(new Pose2d(-35.4,-11.3,Math.toRadians(180)))
+                                .turn(Math.toRadians(-135))
                                 .lineToLinearHeading(farmingPose)
                                 .waitSeconds(0.5)
+                                .lineToLinearHeading(approachPose)
+                                .lineToLinearHeading(rightPark)
+
+
+
 
                                 .build()
                 );
