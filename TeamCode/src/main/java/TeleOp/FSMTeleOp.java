28c49fcd7ffc25fb20af87a2a2eeebd0ec6effb2
Braindeeeaad <Indrajith2048@gmail.com>
2023-01-26 17:07:04 -0600

    BBPancakes-AutoTuning2.0

--- TeamCode/src/main/java/TeleOp/FSMTeleOp.java
+++ TeamCode/src/main/java/TeleOp/FSMTeleOp.java
@@ -183,16 +183,16 @@ public class FSMTeleOp extends OpMode {
 
         if(gamepad2.right_bumper) {
 
-            pulleyMotorL.setPower(0.7*motorPower);
-            pulleyMotorR.setPower(0.7*motorPower);
+            pulleyMotorL.setPower(0.2*motorPower);
+            pulleyMotorR.setPower(0.2*motorPower);
             currentState = State.EMERGENCY;
             targetPosition = pulleyMotorL.getCurrentPosition();
 
         }
         if(gamepad2.left_bumper) {
 
-            pulleyMotorL.setPower(-0.7*motorPower);
-            pulleyMotorR.setPower(-0.7*motorPower);
+            pulleyMotorL.setPower(-0.2*motorPower);
+            pulleyMotorR.setPower(-0.2*motorPower);
             currentState = State.EMERGENCY;
             targetPosition = pulleyMotorL.getCurrentPosition();
 
